e2f7dc144f7e14ad73668bc36d9dfe2ae0948eb4
M. Mendes Moreira <kcamvankester@hotmail.com>
2015-09-22 11:40:19 +0200

    Mack's script
    Dit zijn al mijn scripts die ik gemaakt heb. Ze zijn niet helemaal
    perfect of gefinetuned, maar dit wordt natuurlijk wel later gedaan.

---
+++ Oncomplete Scripts/Player scripts/BreathHolding.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreathHolding : MonoBehaviour {
+	public float curBreath;
+	public float maxBreath;
+	public float breathRegen;
+	public float breathReduce;
+	public float regenRate;
+	public float reduceRate;
+	public float noBreathDamage;
+	float nextReduce;
+	float nextRegen;
+	float minBreath;
+
+	public bool holdBreath;
+	public bool inSafeZone;
+
+	public void Start(){
+		curBreath = maxBreath;
+		minBreath = 0;
+	}
+
+	public void Update(){
+		HoldingBreath ();
+		Reducer ();
+		Regenerator ();
+		Equalizer ();
+	}
+
+	public void HoldingBreath(){
+		if (Input.GetButton ("HoldBreath")) {
+			holdBreath = true;
+		}
+		else{
+			holdBreath = false;
+		}
+	}
+
+	public void Reducer(){
+		if (Time.time > reduceRate + nextReduce && holdBreath) {
+			curBreath -= breathReduce;
+			nextReduce = Time.time;
+			if (curBreath == minBreath) {
+				//GetComponent<PlayerHealth> ().Damage (noBreathDamage);
+			}
+		}
+	}
+
+	public void Regenerator(){
+		if(curBreath < maxBreath && !holdBreath){
+			if(Time.time > regenRate + nextRegen){
+				curBreath += breathRegen;
+				nextRegen = Time.time;
+			}
+		}
+	}
+
+	public void Equalizer(){
+		if(curBreath > maxBreath){
+			curBreath = maxBreath;
+		}
+		if(curBreath < minBreath){
+			curBreath = minBreath;
+		}
+	}
+}
---
+++ Oncomplete Scripts/Player scripts/Defense.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defense : MonoBehaviour {
+	public bool blocken;
+
+	public void Update(){
+		Blocking ();
+	}
+
+	public void Blocking(){
+		if (Input.GetButton ("Fire2")) {
+			blocken = true;
+		}
+		else {
+			blocken = false;
+		}
+	}
+}

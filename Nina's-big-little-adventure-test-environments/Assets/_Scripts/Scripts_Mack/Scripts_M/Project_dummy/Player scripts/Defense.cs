c44fd630f465bd237f490a1ae6f60294d76430ce
M. Mendes Moreira <kcamvankester@hotmail.com>
2015-09-29 14:23:06 +0200

    Defense verandert
    Nu is er ene minimum limiet die ervoor zorgt dat de speler stopt met
    blokken wanneer hij onder een bepaald hoeveelheid stamina zit, ook stopt
    de stamina regen wanneer de speler blokt

--- Nina's-big-little-adventure-test-environments/Assets/_Scripts/Scripts_Mack/Scripts_M/Project_dummy/Player scripts/Defense.cs
+++ Nina's-big-little-adventure-test-environments/Assets/_Scripts/Scripts_Mack/Scripts_M/Project_dummy/Player scripts/Defense.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class Defense : MonoBehaviour {
 	public bool blocken;
 
+	public float minStaminaReq;
+
 	public void Update(){
 		Blocking ();
 	}
 
 	public void Blocking(){
-		if (Input.GetButton ("Fire2")) {
+		float myStamina = GetComponent<Stamina> ().stamina;
+		if (Input.GetButton ("Fire2") && myStamina > minStaminaReq) {
 			blocken = true;
 		}
 		else {
